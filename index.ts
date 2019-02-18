c86860d168b07e2e1b301fc6279a46bd42052675
Ivan Milosevic <contact.ivanmilosevic@gmail.com>
2019-02-19 00:55:32 +0100

    :sparkles: add axios instance for github api

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
+import axios from "axios";
 import dotenv from "dotenv";
 import { schedule } from "node-cron";
 
 dotenv.config();
 
-const { CRON_SCHEDULE } = process.env;
+const { OAUTH_GITHUB, OWNER, REPO, CRON_SCHEDULE } = process.env;
+
+const github = axios.create({
+  baseURL: `https://api.github.com/repos/${OWNER}/${REPO}/git/`,
+  headers: {
+    Authorization: `token ${OAUTH_GITHUB}`
+  }
+});
 
 schedule(
   CRON_SCHEDULE as string,
